ec88b4e4f082974e277f4ef8aafa48b18f8ef142
Filip Dulic <filip.dulic@gmail.com>
2018-12-18 15:13:29 +0100

    Fisrt working async

--- examples/async-pubsub.rs
+++ examples/async-pubsub.rs
@@ -1,15 +1,27 @@
-extern crate tokio;
 extern crate bus_queue;
+extern crate tokio;
 
-use bus_queue::async::Test;
+use bus_queue::async::AsyncBus;
+use std::thread;
+use std::time;
 use tokio::prelude::*;
 
-fn main(){
-    let test = Test::new(); // half a second
-    let future = test.for_each(|curr| {
+fn main() {
+    let mut async_bus: AsyncBus<u32> = AsyncBus::new(10);
+
+    let stream = async_bus.add_sub();
+    let future = stream.for_each(|curr| {
         println!("Counter: {}", curr);
         futures::future::ok(())
     });
+    let a = thread::spawn(move || {
+        thread::sleep(time::Duration::from_millis(2000));
+        for i in 0..40 {
+            async_bus.push(i);
+            thread::sleep(time::Duration::from_millis(500));
+        }
+    });
 
-    tokio::run(future)
+    tokio::run(future);
+    a.join().unwrap();
 }
