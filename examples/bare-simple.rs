44e800454a981b2a6aa2770ac91b5d76f47dee09
Vladan Popovic <vladanovic@gmail.com>
2018-12-25 18:40:35 +0100

    Redo bare-simple to a bit better use-case

--- examples/bare-simple.rs
+++ examples/bare-simple.rs
@@ -4,11 +4,11 @@ use bus_queue::bare_channel;
 
 fn main() {
     let (mut tx, rx) = bare_channel(10);
-    vec![1, 2, 3, 4]
-        .into_iter()
-        .for_each(|x| tx.broadcast(x).unwrap());
+    (1..15).for_each(|x| tx.broadcast(x).unwrap());
 
     let received: Vec<i32> = rx.into_iter().map(|x| *x).collect();
+    // Test that only the last 10 elements are in the received list.
+    let expected: Vec<i32> = (5..15).collect();
 
-    assert_eq!(vec![1, 2, 3, 4], received);
+    assert_eq!(expected, received);
 }
