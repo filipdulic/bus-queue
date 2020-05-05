a6884aa47c72733972f045243c0ac5f328d96ef6
Filip Dulic <filip.dulic@gmail.com>
2020-05-05 22:46:20 +0200

    Refactor wakers to use piper::Event to notify pending streams an item is ready
    Stream Trait is similar to the Future Trait in that it requires internal logic
    to let the executor know when the stream's state changes from Poll::Pending to
    Poll::Ready.
    
    In the original implementation mpsc channel and AtomicWakers were used. The
    AtomicWaker was used to contain the future's context and wake the task, while
    the channel was used to send AtomicWakers from Subscribers to the Publisher.
    After publishing a new item the Publisher collected all the AtomicWakers and
    called the wake method.
    
    This refactor replaces that logic with piper::Event which is awaited in the
    Stream::poll_next method until the Publisher signals that event by calling the
    Event::notify method.
    
    An additional close method was added to channel::Sender which explicitly let's
    all Receivers and Subscribers know that the Sender or Publisher has stopped
    publishing items.

--- src/bus.rs
+++ src/bus.rs
@@ -1,26 +1,23 @@
 use crate::channel::{bounded as raw_bounded, Receiver, SendError, Sender, TryRecvError};
-use crossbeam_channel as mpsc;
-use futures::task::AtomicWaker;
-use futures::{
-    task::{self, Poll},
-    Sink, Stream,
-};
+use futures::{task::{self, Poll}, Sink, Stream};
+use futures::future::Future;
 use std::pin::Pin;
 use std::sync::Arc;
+use piper::{Event, EventListener};
+
 
 pub fn bounded<T>(size: usize) -> (Publisher<T>, Subscriber<T>) {
     let (sender, receiver) = raw_bounded(size);
-    let (waker, sleeper) = alarm();
+    let event = Arc::new(Event::new());
     (
-        Publisher { sender, waker },
-        Subscriber { receiver, sleeper },
+        Publisher { sender, event: event.clone() },
+        Subscriber { receiver, event, listener: None },
     )
 }
 
-#[derive(Debug)]
 pub struct Publisher<T> {
     sender: Sender<T>,
-    waker: Waker,
+    event: Arc<Event>,
 }
 
 impl<T> Sink<T> for Publisher<T> {
@@ -33,10 +30,8 @@ impl<T> Sink<T> for Publisher<T> {
         Poll::Ready(Ok(()))
     }
 
-    fn start_send(mut self: Pin<&mut Self>, item: T) -> Result<(), Self::Error> {
-        self.waker.collect_new_wakers();
+    fn start_send(self: Pin<&mut Self>, item: T) -> Result<(), Self::Error> {
         self.sender.broadcast(item).and_then(|_| {
-            self.waker.wake_all();
             Ok(())
         })
     }
@@ -45,14 +40,16 @@ impl<T> Sink<T> for Publisher<T> {
         self: Pin<&mut Self>,
         _: &mut task::Context<'_>,
     ) -> Poll<Result<(), Self::Error>> {
+        self.event.notify_all();
         Poll::Ready(Ok(()))
     }
 
     fn poll_close(
         self: Pin<&mut Self>,
-        _: &mut task::Context<'_>,
+        cx: &mut task::Context<'_>,
     ) -> Poll<Result<(), Self::Error>> {
-        Poll::Ready(Ok(()))
+        self.sender.close();
+        self.poll_flush(cx)
     }
 }
 
@@ -62,11 +59,26 @@ impl<T> PartialEq for Publisher<T> {
     }
 }
 
+impl<T> Drop for Publisher<T> {
+    fn drop(&mut self) {
+        self.sender.close();
+        self.event.notify_all();
+    }
+}
+
 impl<T> Eq for Publisher<T> {}
 
 pub struct Subscriber<T> {
     receiver: Receiver<T>,
-    sleeper: Sleeper,
+    event: Arc<Event>,
+    listener: Option<EventListener>,
+}
+
+impl<T> std::fmt::Debug for Subscriber<T>{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result{
+        f.debug_struct("Subscriber")
+            .finish()
+    }
 }
 
 impl<T> Subscriber<T> {
@@ -78,14 +90,40 @@ impl<T> Subscriber<T> {
 impl<T> Stream for Subscriber<T> {
     type Item = Arc<T>;
 
-    fn poll_next(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<Option<Self::Item>> {
-        self.sleeper.register(cx.waker());
-        match self.receiver.try_recv() {
-            Ok(item) => Poll::Ready(Some(item)),
-            Err(error) => match error {
-                TryRecvError::Empty => Poll::Pending,
-                TryRecvError::Disconnected => Poll::Ready(None),
-            },
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<Option<Self::Item>> {
+        loop {
+            // If this stream is blocked on an event, first make sure it is unblocked.
+            if let Some(listener) = self.listener.as_mut() {
+                futures::ready!(Pin::new(listener).poll(cx));
+                self.listener = None;
+            }
+            loop {
+                // Attempt to receive a message.
+                match self.receiver.try_recv() {
+                    Ok(item) => {
+                        // The stream is not blocked on an event - drop the listener.
+                        self.listener = None;
+                        return Poll::Ready(Some(item));
+                    },
+                    Err(TryRecvError::Disconnected) => {
+                        // The stream is not blocked on an event - drop the listener.
+                        self.listener = None;
+                        return Poll::Ready(None);
+                    },
+                    Err(TryRecvError::Empty) => {},
+                }
+                // Listen for a send event.
+                match self.listener.as_mut() {
+                    None => {
+                        // Store a listener and try sending the message again.
+                        self.listener = Some(self.event.listen())
+                    },
+                    Some(_) => {
+                        // Go back to the outer loop to poll the listener.
+                        break;
+                    }
+                }
+            }
         }
     }
 }
@@ -94,7 +132,8 @@ impl<T> Clone for Subscriber<T> {
     fn clone(&self) -> Self {
         Self {
             receiver: self.receiver.clone(),
-            sleeper: self.sleeper.clone(),
+            event: self.event.clone(),
+            listener: None,
         }
     }
 }
@@ -107,69 +146,6 @@ impl<T: Send> PartialEq for Subscriber<T> {
 
 impl<T: Send> Eq for Subscriber<T> {}
 
-// Helper struct used by sync and async implementations to wake Tasks / Threads
-#[derive(Debug)]
-pub struct Waker {
-    /// Vector of Wakers to use to wake up subscribers.
-    wakers: Vec<Arc<AtomicWaker>>,
-    /// A mpsc Receiver used to receive Wakers
-    receiver: mpsc::Receiver<Arc<AtomicWaker>>,
-}
-
-impl Waker {
-    fn wake_all(&self) {
-        for waker in &self.wakers {
-            waker.wake();
-        }
-    }
-
-    /// Receive any new Wakers and add them to the wakers Vec. These will be used to wake up the
-    /// subscribers when a message is published
-    fn collect_new_wakers(&mut self) {
-        while let Ok(receiver) = self.receiver.try_recv() {
-            self.wakers.push(receiver);
-        }
-    }
-}
-
-/// Helper struct used by sync and async implementations to register Tasks / Threads to
-/// be woken up.
-#[derive(Debug)]
-pub struct Sleeper {
-    /// Current Waker to be woken up
-    waker: Arc<AtomicWaker>,
-    /// mpsc Sender used to send Wakers to the Publisher
-    sender: mpsc::Sender<Arc<AtomicWaker>>,
-}
-
-impl Sleeper {
-    fn register(&self, waker: &task::Waker) {
-        self.waker.register(waker);
-    }
-}
-
-impl Clone for Sleeper {
-    fn clone(&self) -> Self {
-        let waker = Arc::new(AtomicWaker::new());
-        // Send the new waker to the publisher.
-        // If this fails (Receiver disconnected), presumably the Publisher
-        // has dropped and when this is polled for the first time, the
-        // Stream will end.
-        let _ = self.sender.send(Arc::clone(&waker));
-        Self {
-            waker,
-            sender: self.sender.clone(),
-        }
-    }
-}
-
-/// Function used to create a ( Waker, Sleeper ) tuple.
-pub fn alarm() -> (Waker, Sleeper) {
-    let (sender, receiver) = mpsc::unbounded();
-    let waker = Arc::new(AtomicWaker::new());
-    let wakers = vec![Arc::clone(&waker)];
-    (Waker { wakers, receiver }, Sleeper { waker, sender })
-}
 
 #[cfg(test)]
 mod test {
@@ -184,8 +160,7 @@ mod test {
     use futures::future::FutureExt;
 
     #[test]
-    fn subscriber_is_in_pending_state_before_first_data_is_published()
-    {
+    fn subscriber_is_in_pending_state_before_first_data_is_published() {
         let (_publisher,  subscriber) = super::bounded::<usize>(1);
         pin_mut!(subscriber);
 
@@ -194,8 +169,7 @@ mod test {
     }
 
     #[test]
-    fn subscriber_receives_an_item_after_it_is_published()
-    {
+    fn subscriber_receives_an_item_after_it_is_published() {
         let mut cx = noop_context();
         let (publisher,  subscriber) = super::bounded::<usize>(1);
         pin_mut!(subscriber);
@@ -207,15 +181,15 @@ mod test {
         // Assert that the subscriber can receive item (1).
         assert_stream_next!(subscriber, Arc::new(1));
     }
+
     #[test]
-    fn subscriber_recieves_an_item_after_publisher_overflowed()
-    {
+    fn subscriber_recieves_an_item_after_publisher_overflowed() {
         let mut cx = noop_context();
         let (publisher,  subscriber) = super::bounded::<usize>(1);
         pin_mut!(subscriber);
         pin_mut!(publisher);
 
-        // Publish 1 item (1).
+        // Publish item (1).
         assert_eq!(publisher.send(1).poll_unpin(&mut cx), Poll::Ready(Ok(())));
 
         // Assert that the publisher is not blocked even when overflowed
@@ -227,8 +201,7 @@ mod test {
         assert_stream_next!(subscriber, Arc::new(2));
     }
     #[test]
-    fn subscriber_is_done_after_publisher_closes()
-    {
+    fn subscriber_is_done_after_publisher_closes() {
         let mut cx = noop_context();
         let (publisher,  subscriber) = super::bounded::<usize>(1);
         pin_mut!(subscriber);
@@ -237,13 +210,25 @@ mod test {
         // Close Publisher.
         assert_eq!(publisher.close().poll_unpin(&mut cx), Poll::Ready(Ok(())));
 
-        // Assert that the subscriber can receive item (1).
+        // Assert that the subscriber is done..
         assert_stream_done!(subscriber);
     }
 
     #[test]
-    fn notify()
-    {
+    fn subscriber_is_done_after_publisher_drop() {
+        let (publisher,  subscriber) = super::bounded::<usize>(1);
+        pin_mut!(subscriber);
+
+        // Drop Publisher
+        drop(publisher);
+
+
+        // Assert that the subscriber is done.
+        assert_stream_done!(subscriber);
+    }
+
+    #[test]
+    fn notify() {
         let (publisher,  subscriber) = super::bounded::<usize>(1);
         pin_mut!(subscriber);
         pin_mut!(publisher);
